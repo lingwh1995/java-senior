48e3a8cc6bb0b1deca2af36fb743be5d1cd08aaf
lingwh <LRwq942133>
2023-12-07 20:52:38 +0800

    初始化

---
+++ algorithm/src/main/java/org/bluebridge/algorithm/joseph/JosephCircleTest.java
@@ -0,0 +1,23 @@
+package org.bluebridge.algorithm.joseph;
+
+
+import org.junit.Test;
+
+public class JosephCircleTest {
+
+    /**
+     * 测试数组实现约瑟夫环
+     */
+    @Test
+    public void testJosephCircileArray() {
+        JosephCircle.josephCircileArray(10, 3);
+    }
+
+    /**
+     * 测试ArrayList实现约瑟夫环
+     */
+    @Test
+    public void testJosephCircileArrayList() {
+        JosephCircle.josephCircileArrayList(5, 2);
+    }
+}
---
+++ algorithm/src/main/java/org/bluebridge/algorithm/sort/InsertSort.java
@@ -0,0 +1,33 @@
+package org.bluebridge.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 插入排序
+ * @author lingwh
+ *
+ */
+public class InsertSort {
+	
+	/**
+	 * 插入排序:内存循环最好使用while,方便设置多个循环条件
+	 */
+	public static void main(String[] args) {
+		int[] nums = {3,1,5,2};
+		for(int i=0; i<nums.length; i++) {
+			int insertValue = nums[i];
+			//初始的insertValue索引的前一位
+			int insertIndex = i-1;
+			while(insertIndex >=0 && insertValue < nums[insertIndex]) {
+				nums[insertIndex+1] = nums[insertIndex];
+				insertIndex--;
+			}
+			//为什么insertIndex要+1,因为第i和数第一次和第i-1个数做对比,此时的索引指向的是第i-1个数,--后指向的是第i-1个数的前一个数,所以最终找到的是索引的前一个位置,所以插入时要+1
+			if(nums[insertIndex+1] != insertValue) {
+				nums[insertIndex+1] = insertValue;
+			}
+		}
+		System.out.println(Arrays.toString(nums));
+	}
+	
+}
