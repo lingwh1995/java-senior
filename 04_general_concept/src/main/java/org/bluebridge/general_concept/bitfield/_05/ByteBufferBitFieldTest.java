c8116fbf98aee93b339c9a2268ab842ceea2ac1f
think <lingwh.work@hotmail.com>
2026-04-07 18:06:16 +0800

    修改项目结构

---
+++ 04_general_concept/src/main/java/org/bluebridge/general_concept/bitfield/_05/ByteBufferBitFieldTest.java
@@ -0,0 +1,29 @@
+package org.bluebridge.general_concept.bitfield._05;
+
+/**
+ * @author lingwh
+ * @desc
+ * @date 2026/4/4 20:47
+ */
+public class ByteBufferBitFieldTest {
+
+    public static void main(String[] args) {
+        ByteBufferBitField bitField = new ByteBufferBitField();
+
+        // 每次 set 都必须重新赋值！
+        bitField.set(0, 1, 1);
+        System.out.println("S1: " + bitField.get(0, 1));
+
+        bitField.set(1, 2, 3);
+        System.out.println("S2: " + bitField.get(1, 2));
+
+        bitField.set(3, 5, 31);
+        System.out.println("S3: " + bitField.get(3, 5));
+
+        System.out.println("\nBytes: ");
+        for (byte b : bitField.toBytes()) {
+            System.out.printf("%02X ", b);
+        }
+    }
+
+}
